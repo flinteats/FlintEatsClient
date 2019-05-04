72ef043e14d1a1b59fbe4975a069bbf2ceea0410
Tom Lowe <musicaltommy@gmail.com>
2019-05-03 21:42:58 -0400

    Fixed/Improved buttons on the Feed page

--- src/routes/feed-header.js
+++ src/routes/feed-header.js
@@ -11,6 +11,13 @@ const deal0 = require('../../res/deal0.png');
 const deal1 = require('../../res/deal1.png');
 const tip0 = require('../../res/tip0.png');
 const tip1 = require('../../res/tip1.png');
+const recipe0 = require('../../res/recipe0.png');
+const recipe1 = require('../../res/recipe1.png');
+const review0 = require('../../res/review0.png');
+const review1 = require('../../res/review1.png');
+
+const buttonsmalldiameter = 45;
+const buttonbigdiameter = 50;
 
 class FeedHeaderView extends React.Component {
   constructor(props) {
@@ -30,64 +37,125 @@ class FeedHeaderView extends React.Component {
   render() {
     return (
       <LinearGradient
-          start={{x: 0, y: 0.5}}
-          end={{x: 1, y: 0.5}}
-          colors={['#ABE894', '#54E085']}>
+        start={{ x: 0, y: 0.5 }}
+        end={{ x: 1, y: 0.5 }}
+        colors={['#ABE894', '#54E085']}
+        style={{height:80}}>
         <View style={styles.searchBar}>
-          <View style={{flex: 1, flexDirection: 'row', alignContent: 'center'}}>
+          <View style={{ flex: 1, flexDirection: 'row', alignContent: 'center' }}>
             <Left>
-              <TouchableOpacity style={{flex: 1, margin: 6}}
-                  onPress={() => this.search(this.props.queryText)}>
+              <TouchableOpacity style={{ flex: 1, margin: 6 }}
+                onPress={() => this.search(this.props.queryText)}>
                 <Icon name='search' />
               </TouchableOpacity>
             </Left>
-            <TextInput style={{flex: 6}}
-                onChangeText={(text) => this.props.setQueryText(text)}
-                onSubmitEditing={() => this.search(this.props.queryText)}
-                placeholder={'Search'}
-                value={this.props.queryText}
+            <TextInput style={{ flex: 6 }}
+              onChangeText={(text) => this.props.setQueryText(text)}
+              onSubmitEditing={() => this.search(this.props.queryText)}
+              placeholder={'Search'}
+              value={this.props.queryText}
             />
             <Right>
-              <TouchableOpacity style={{flex: 1, margin: 6}}
-                  onPress={() => this.clear()}>
-                <Icon style={{paddingRight: 10}} name='close' />
+              <TouchableOpacity style={{ flex: 1, margin: 6 }}
+                onPress={() => this.clear()}>
+                <Icon style={{ paddingRight: 10 }} name='close' />
               </TouchableOpacity>
             </Right>
           </View>
         </View>
-        <View style={styles.buttonContainer}>
+        {/* <View style={styles.buttonContainer}>
           <TouchableOpacity style={styles.button}
-              accessibilityLabel='tips'
-              onPress={() => this.props.setFocus('tip')}>
+            accessibilityLabel='tips'
+            onPress={() => this.props.setFocus('tip')}>
             <Image
-                style={{width: 40, height: 40}}
-                source={this.props.focus == 'tip'
-                          ? tip1
-                          : tip0}
+              style={{ width: 40, height: 40 }}
+              source={this.props.focus == 'tip'
+                ? tip1
+                : tip0}
             />
           </TouchableOpacity>
           <TouchableOpacity style={styles.button}
-              accessibilityLabel='deals'
-              onPress={() => this.props.setFocus('deal')}>
+            accessibilityLabel='deals'
+            onPress={() => this.props.setFocus('deal')}>
             <Image
-                style={{width: 40, height: 40}}
-                source={this.props.focus == 'deal'
-                          ? deal1
-                          : deal0}
+              style={{ width: 40, height: 40 }}
+              source={this.props.focus == 'deal'
+                ? deal1
+                : deal0}
             />
           </TouchableOpacity>
-{false ? <View>
-          <TouchableOpacity style={styles.button}
+          <View>
+            <TouchableOpacity style={styles.button}
               accessibilityLabels='reviews'
               onPress={this.props.handler}>
-            <Icon name='document' />
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button}
+              <Icon name='document' />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button}
               accessibilityLabels='recipes'
               onPress={this.props.handler}>
-            <Icon name='beaker' />
+              <Icon name='beaker' />
+            </TouchableOpacity>
+          </View>
+        </View> */}
+        {/* This view is the tips, deals, reviews, and recipes buttons */}
+        <View style={styles.buttonContainer}>
+
+          <TouchableOpacity accessibilityLabel='tips' onPress={() => this.props.setFocus('tip')}>
+
+            {this.props.focus == 'tip' ?
+              <View style={styles.buttonWrapperView}>
+                <Image style={styles.bigButton} source={tip1} />
+              </View>
+              :
+              <View style={styles.buttonWrapperView}>
+                <Image style={styles.smallButton} source={tip0} />
+              </View>
+            }
+
           </TouchableOpacity>
-</View> : null}
+
+          <TouchableOpacity accessibilityLabel='deals' onPress={() => this.props.setFocus('deal')}>
+
+            {this.props.focus == 'deal' ?
+              <View style={styles.buttonWrapperView}>
+                <Image style={styles.bigButton} source={deal1} />
+              </View>
+              :
+              <View style={styles.buttonWrapperView}>
+                <Image style={styles.smallButton} source={deal0} />
+              </View>
+            }
+
+          </TouchableOpacity>
+
+          <TouchableOpacity accessibilityLabel='reviews' onPress={() => this.props.setFocus('review')}>
+
+            {this.props.focus == 'review' ?
+              <View style={styles.buttonWrapperView}>
+                <Image style={styles.bigButton} source={review1} />
+              </View>
+              :
+              <View style={styles.buttonWrapperView}>
+                <Image style={styles.smallButton} source={review0} />
+              </View>
+            }
+
+          </TouchableOpacity>
+
+          <TouchableOpacity accessibilityLabel='recipes' onPress={() => this.props.setFocus('recipe')}>
+
+            {this.props.focus == 'recipe' ?
+              <View style={styles.buttonWrapperView}>
+                <Image style={styles.bigButton} source={recipe1} />
+              </View>
+              :
+              <View style={styles.buttonWrapperView}>
+                <Image style={styles.smallButton} source={recipe0} />
+              </View>
+            }
+
+          </TouchableOpacity>
+
         </View>
       </LinearGradient>
     );
@@ -106,9 +174,31 @@ const styles = StyleSheet.create({
     height: 40
   },
   buttonContainer: {
+    position: 'absolute',
+    top:55,
+    left:0,
+    width: '100%',
     flexDirection: 'row',
     justifyContent: 'space-around',
-    height: 45
+    height: buttonbigdiameter,
+  },
+  buttonWrapperView: {
+    height: buttonbigdiameter, 
+    width: buttonbigdiameter, 
+    justifyContent:'center', 
+    alignItems:'center'
+  },
+  bigButton: {
+    height: buttonbigdiameter,
+    width: buttonbigdiameter,
+    justifyContent: 'center',
+    alignItems:'center'
+  },
+  smallButton: {
+    height: buttonsmalldiameter,
+    width: buttonsmalldiameter,
+    justifyContent: 'center',
+    alignItems:'center'
   },
   button: {
     alignContent: 'center',
